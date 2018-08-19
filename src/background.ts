b08fdb81f0b1c878f7291206da8188a604c68cc1
Brian Cui <brian.cui@live.com>
2018-08-18 17:29:01 -0700

    Fixeds for Chrome compat

--- src/background.ts
+++ src/background.ts
@@ -1,17 +1,16 @@
 import { browser } from "webextension-polyfill-ts";
 
 browser.browserAction.onClicked.addListener(async (tab) => {
-    console.log("Duck Clicked!");
     const tabs = await browser.tabs.query({
         active: true,
         currentWindow: true,
     });
 
     await browser.tabs.insertCSS({
-        file: browser.extension.getURL("content/content.css"),
+        file: "content/content.css",
     });
     await browser.tabs.executeScript({
-        file: browser.extension.getURL("content/content.js"),
+        file: "content/content.js",
     });
 
     browser.tabs.sendMessage(tabs[0].id, {
